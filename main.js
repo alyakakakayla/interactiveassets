49829d10b4ea2761aba6dae54fa2f37892f59303
kayla <jadefoxx01@gmail.com>
2024-03-11 18:21:27 -0400

    Butterflies interactive

--- main.js
+++ main.js
@@ -1,15 +1,84 @@
 import * as THREE from 'three';
+import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
+import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
+let mouseX = window.innerWidth/2;
+let mouseY = window.innerHeight/2;
+let object;
+let controls;
+const gltfLoader = new GLTFLoader();
+let mixer;
+
+gltfLoader.load(
+    './images/butterflies/scene.gltf',
+    function (gltf) {
+        object = gltf.scene;
+        object.position.set(-3, -.5, 0);
+        scene.add(object);
+        mixer = new THREE.AnimationMixer(object);
+        const clips = gltf.animations;
+        // const clip = THREE.AnimationClip.findByName(clips, 'HeadAction');
+        // const action = mixer.clipAction(clip);
+        // action.play();
+        clips.forEach(function(clip) {
+            const action = mixer.clipAction(clip);
+            action.play();
+        })
+    },
+    function (xhr) {
+        console.log((xhr.loaded / xhr.total*100) + '% loaded');
+    },
+    function (error) {
+        console.error(error);
+    }
+);
+
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
-document.body.appendChild(renderer.domElement);
+document.getElementById('canvas-container').appendChild(renderer.domElement);
 
+camera.position.z = 300;
+
+const topLeftLight = new THREE.DirectionalLight(0x9cd2ff, 6); //color, intensity
+topLeftLight.position.set(500,500,500);
+topLeftLight.castShadow = true;
+scene.add(topLeftLight);
+
+const topLight = new THREE.DirectionalLight(0xFFFFFF, 5);
+scene.add(topLight);
+
+const bottomLight = new THREE.DirectionalLight(0xfcd792, 3);
+bottomLight.position.set(500,-500,500);
+scene.add(bottomLight);
+
+controls = new OrbitControls(camera, renderer.domElement);
+
+const clock = new THREE.Clock();
+function animate() {
+    if(mixer)
+    mixer.update(clock.getDelta());
+    requestAnimationFrame(animate);
+    renderer.render(scene, camera);
+}
+
+window.addEventListener('resize', function() {
+    camera.aspect = window.innerWidth/window.innerHeight;
+    camera.updateProjectionMatrix();
+    renderer.setSize(window.innerWidth, window.innerHeight);
+});
+
+animate();
+
+/*
 const geometry = new THREE.BoxGeometry(1, 1, 1);
 const material = new THREE.MeshBasicMaterial( {color: 0x00ff00});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube); //coords 0, 0, 0
+
+
+
 
 camera.position.z = 5;
 
@@ -20,3 +89,4 @@ function animate() {
 	renderer.render(scene, camera);
 }
 animate();
+*/
