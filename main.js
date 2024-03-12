04d46e136c3d6a4ba8e4b1739c93c702896e586e
kayla <jadefoxx01@gmail.com>
2024-03-12 15:49:51 -0400

    Add multiple assets

--- main.js
+++ main.js
@@ -2,17 +2,231 @@ import * as THREE from 'three';
 import {GLTFLoader} from 'three/addons/loaders/GLTFLoader.js';
 import {OrbitControls} from 'three/addons/controls/OrbitControls.js';
 
+document.getElementById('first-message').style.display="block";
+document.getElementById('butterflies-credits').style.display="none";
+document.getElementById('blackhole-credits').style.display="none";
+document.getElementById('earth-credits').style.display="none";
+document.getElementById('gits-credits').style.display="none";
+
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth/window.innerHeight, 0.1, 1000);
 let mouseX = window.innerWidth/2;
 let mouseY = window.innerHeight/2;
 let object;
+let selectedObject = "butterflies";
 let controls;
 const gltfLoader = new GLTFLoader();
 let mixer;
 
+document.getElementById("butterflies-button").addEventListener("click", loadButterflies);
+document.getElementById("blackhole-button").addEventListener("click", loadBlackhole);
+document.getElementById("earth-button").addEventListener("click", loadEarth);
+document.getElementById("gits-button").addEventListener("click", loadGits);
+
+function loadButterflies() {
+    document.getElementById('first-message').style.display="none";
+    document.getElementById('butterflies-credits').style.display="block";
+    document.getElementById('blackhole-credits').style.display="none";
+    document.getElementById('earth-credits').style.display="none";
+    document.getElementById('gits-credits').style.display="none";
+
+    while(scene.children.length > 0){ 
+        scene.remove(scene.children[0]); 
+    }
+
+    selectedObject = "butterflies";
+
+    gltfLoader.load(
+        `./images/${selectedObject}/scene.gltf`,
+        function (gltf) {
+            object = gltf.scene;
+            object.position.set(-50, -.7, 0);
+            scene.add(object);
+            mixer = new THREE.AnimationMixer(object);
+            const clips = gltf.animations;
+            // const clip = THREE.AnimationClip.findByName(clips, 'HeadAction');
+            // const action = mixer.clipAction(clip);
+            // action.play();
+            clips.forEach(function(clip) {
+                const action = mixer.clipAction(clip);
+                action.play();
+            })
+        },
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total*100) + '% loaded');
+        },
+        function (error) {
+            console.error(error);
+        }
+    );
+
+    const topLeftLight = new THREE.DirectionalLight(0xFFFFFF, 6); //color, intensity
+    topLeftLight.position.set(500,500,500);
+    topLeftLight.castShadow = true;
+    scene.add(topLeftLight);
+
+    const topLight = new THREE.DirectionalLight(0xFFFFFF, 5);
+    scene.add(topLight);
+
+    const bottomLight = new THREE.DirectionalLight(0xFFFFFF, 3);
+    bottomLight.position.set(500,-500,500);
+    scene.add(bottomLight);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+}
+
+function loadBlackhole() {
+    document.getElementById('first-message').style.display="none";
+    document.getElementById('butterflies-credits').style.display="none";
+    document.getElementById('blackhole-credits').style.display="block";
+    document.getElementById('earth-credits').style.display="none";
+    document.getElementById('gits-credits').style.display="none";
+
+    while(scene.children.length > 0){ 
+        scene.remove(scene.children[0]); 
+    }
+
+    selectedObject = "blackhole";
+
+    gltfLoader.load(
+        `./images/${selectedObject}/scene.gltf`,
+        function (gltf) {
+            object = gltf.scene;
+            object.position.set(0, -.7, 0);
+            object.scale.set(100, 100, 100);
+            scene.add(object);
+            mixer = new THREE.AnimationMixer(object);
+            const clips = gltf.animations;
+            // const clip = THREE.AnimationClip.findByName(clips, 'HeadAction');
+            // const action = mixer.clipAction(clip);
+            // action.play();
+            clips.forEach(function(clip) {
+                const action = mixer.clipAction(clip);
+                action.play();
+            })
+        },
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total*100) + '% loaded');
+        },
+        function (error) {
+            console.error(error);
+        }
+    );
+
+    const topLeftLight = new THREE.DirectionalLight(0xFFFFFF, 6); //color, intensity
+    topLeftLight.position.set(500,500,500);
+    topLeftLight.castShadow = true;
+    scene.add(topLeftLight);
+
+    const topLight = new THREE.DirectionalLight(0xFFFFFF, 5);
+    scene.add(topLight);
+
+    const bottomLight = new THREE.DirectionalLight(0xFFFFFF, 3);
+    bottomLight.position.set(500,-500,500);
+    scene.add(bottomLight);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+}
+
+function loadEarth() {
+    document.getElementById('first-message').style.display="none";
+    document.getElementById('first-message').style.display="none;"
+    document.getElementById('butterflies-credits').style.display="none";
+    document.getElementById('blackhole-credits').style.display="none";
+    document.getElementById('earth-credits').style.display="block";
+    document.getElementById('gits-credits').style.display="none";
+
+    while(scene.children.length > 0){ 
+        scene.remove(scene.children[0]); 
+    }
+    
+    selectedObject = "earth";
+
+    gltfLoader.load(
+        `./images/${selectedObject}/scene.gltf`,
+        function (gltf) {
+            object = gltf.scene;
+            object.position.set(0, -.7, 0);
+            object.scale.set(100, 100, 100);
+            scene.add(object);
+            mixer = new THREE.AnimationMixer(object);
+            const clips = gltf.animations;
+            // const clip = THREE.AnimationClip.findByName(clips, 'HeadAction');
+            // const action = mixer.clipAction(clip);
+            // action.play();
+            clips.forEach(function(clip) {
+                const action = mixer.clipAction(clip);
+                action.play();
+            })
+        },
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total*100) + '% loaded');
+        },
+        function (error) {
+            console.error(error);
+        }
+    );
+
+    const topLeftLight = new THREE.DirectionalLight(0xFFFFFF, 6); //color, intensity
+    topLeftLight.position.set(500,500,500);
+    topLeftLight.castShadow = true;
+    scene.add(topLeftLight);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+}
+
+function loadGits() {
+    document.getElementById('first-message').style.display="none";
+    document.getElementById('butterflies-credits').style.display="none";
+    document.getElementById('blackhole-credits').style.display="none";
+    document.getElementById('earth-credits').style.display="none";
+    document.getElementById('gits-credits').style.display="block";
+
+    while(scene.children.length > 0){ 
+        scene.remove(scene.children[0]); 
+    }
+
+    selectedObject = "gits";
+
+    gltfLoader.load(
+        `./images/${selectedObject}/scene.gltf`,
+        function (gltf) {
+            object = gltf.scene;
+            object.position.set(0, -50, 0);
+            object.scale.set(160, 160, 160);
+            object.rotateY(180);
+            scene.add(object);
+            mixer = new THREE.AnimationMixer(object);
+            const clips = gltf.animations;
+            // const clip = THREE.AnimationClip.findByName(clips, 'HeadAction');
+            // const action = mixer.clipAction(clip);
+            // action.play();
+            clips.forEach(function(clip) {
+                const action = mixer.clipAction(clip);
+                action.play();
+            })
+        },
+        function (xhr) {
+            console.log((xhr.loaded / xhr.total*100) + '% loaded');
+        },
+        function (error) {
+            console.error(error);
+        }
+    );
+
+    const topLight = new THREE.DirectionalLight(0xFFFFFF, 5);
+    scene.add(topLight);
+
+    const bottomLight = new THREE.DirectionalLight(0xFFFFFF, 5);
+    bottomLight.position.set(500,-500,500);
+    scene.add(bottomLight);
+
+    controls = new OrbitControls(camera, renderer.domElement);
+}
+
+/*
 gltfLoader.load(
-    './images/butterflies/scene.gltf',
+    `./images/${selectedObject}/scene.gltf`,
     function (gltf) {
         object = gltf.scene;
         object.position.set(0, -.7, 0);
@@ -34,26 +248,13 @@ gltfLoader.load(
         console.error(error);
     }
 );
+*/
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.getElementById('canvas-container').appendChild(renderer.domElement);
 
 camera.position.z = 300;
-
-const topLeftLight = new THREE.DirectionalLight(0xFFFFFF, 6); //color, intensity
-topLeftLight.position.set(500,500,500);
-topLeftLight.castShadow = true;
-scene.add(topLeftLight);
-
-const topLight = new THREE.DirectionalLight(0xFFFFFF, 5);
-scene.add(topLight);
-
-const bottomLight = new THREE.DirectionalLight(0xFFFFFF, 3);
-bottomLight.position.set(500,-500,500);
-scene.add(bottomLight);
-
-controls = new OrbitControls(camera, renderer.domElement);
 
 const clock = new THREE.Clock();
 function animate() {
